d5e5490567555c35530a9b1dee3d2bb98d100268
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-05-26 22:38:39 -0300

    renamed folder

---
+++ contamination_model/config.py
@@ -0,0 +1,7 @@
+from os import path
+import contamination_model
+# Paths
+base_path = path.dirname(path.dirname(contamination_model.__file__))
+workspace_path = path.join(base_path, 'workspace')
+data_path = path.join(workspace_path, 'data')
+models_path = path.join(workspace_path, 'models')
