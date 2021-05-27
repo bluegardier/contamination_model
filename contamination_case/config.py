5c55c5c553a1f2dc13087f8e6a2e5991858d2a9c
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-05-26 22:38:08 -0300

    removed files

--- contamination_case/config.py
+++
@@ -1,7 +0,0 @@
-from os import path
-import contamination_case
-# Paths
-base_path = path.dirname(path.dirname(contamination_case.__file__))
-workspace_path = path.join(base_path, 'workspace')
-data_path = path.join(workspace_path, 'data')
-models_path = path.join(workspace_path, 'models')
