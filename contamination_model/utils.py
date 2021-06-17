16f501958e72d8a231c6d05332ef1d095876f4da
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-17 17:48:54 -0300

    utils for data analysis

---
+++ contamination_model/utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_configuration(x: float = 11.7, y: float = 8.27) -> None:
+    """
+    Fixed configuration for seaborn plots.
+
+    Parameters
+    ----------
+    x : float, optional
+        The plot's X-axis, by default 11.7
+    y : float, optional
+        The plot's X-axis, by default 8.27
+    """
+    a4_dims = (x, y)
+    plt.subplots(figsize=a4_dims)
+
+
+def plotting_categories(df: pd.DataFrame, column: str) -> None:
+    """
+    Distribution Plot for the selected column, 
+    filtering by variable.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A dataframe with a category column.
+    column : str
+        The category column for plotting.
+    """
+    categories = df[column].unique()
+
+    for category in categories:
+        series_to_plot = df[df[column] == category]['prob_V1_V2']
+        sns.distplot(series_to_plot, hist=False, label=category)
